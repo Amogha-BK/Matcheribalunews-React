cb992f3533c98e5fa9557bbf2d069f0b095072f0
Amoghbk <amoghbk42@gmail.com>
2026-06-07 16:30:09 +0530

    Logo resized

--- src/shared/Logo.tsx
+++ src/shared/Logo.tsx
@@ -8,7 +8,7 @@ interface Props extends React.SVGProps<SVGSVGElement> {
   size?: string
 }
 
-const Logo: React.FC<Props> = ({ className, size = 'size-12 sm:size-14', ...props }) => {
+const Logo: React.FC<Props> = ({ className, size = 'size-20 sm:size-18', ...props }) => {
   return (
     <Link href="/" className={clsx('inline-block shrink-0 text-primary-800 dark:text-primary-900', className, size)}>
   <Image
